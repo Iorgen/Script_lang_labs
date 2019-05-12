7b065e82e7cc55abeb0997fcc069e03221b13618
Jurgen <iorgenkras@gmail.com>
2019-05-13 00:29:37 +0700

    Perl bug fix

--- Perl/task.pl
+++ Perl/task.pl
@@ -1,24 +1,19 @@
 #!/usr/bin/perl
-
 open(INPUT_FILE, "< access.log");
-
-%result_ips = ();
-
+%ip_list = ();
 while ($line = <INPUT_FILE>) {
     if ($line =~ m/(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/) {
-        if (exists $result_ips{$1}) {
-            $result_ips{$1}++;
+        if (exists $ip_list{$1}) {
+            $ip_list{$1}++;
         }
         else {
-            $result_ips{$1} = 1;
+            $ip_list{$1} = 1;
         }
     }
 }
-
-@sorted_result = sort {$result_ips{$a} cmp $result_ips{$b}} keys %result_ips ;
-
+@sorted_ip_list = sort {$sorted_ip_list{$a} cmp $sorted_ip_list{$b}} keys %sorted_ip_list ;
 for (my $i=0; $i < 10; $i++) {
-    print $sorted_result[$i], "\n";
+    print $sorted_ip_list[$i], "\n";
 }
 
 
